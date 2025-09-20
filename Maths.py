8e7b893eb113ca5ad6d35f32addf833884a58032
imayanknegi29 <imayanknegi29@gmail.com>
2025-09-20 13:33:45 +0530

    math library

---
+++ Maths.py
@@ -0,0 +1,49 @@
+
+# augmented operator
+
+friend = 5
+# friend += 1
+# friend -= 1
+# friend *= 3
+# friend /= 2
+# friend **= 2
+friend %= 2
+
+print(friend)
+
+# Round function
+x = 3.14
+y = -4
+z = 5
+
+result = round(x)
+print(result)
+
+absoluteValue = abs(y)
+print(absoluteValue)
+
+cube = pow(z, 3)
+print(cube)
+
+maximum = max(x, y, z)
+print(maximum)
+
+minimum = min(x, y, z)
+print(minimum)
+
+import math
+
+pi = math.pi
+print(pi)
+
+exponential = math.e
+print(exponential)
+
+sqrt = math.sqrt(9)
+print(sqrt)
+
+ceiling = math.ceil(x)
+print(ceiling)
+
+floor = math.floor(x)
+print(floor)
