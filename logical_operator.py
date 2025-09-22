19b7a262c4638bc082381881597978fddea30e99
imayanknegi29 <imayanknegi29@gmail.com>
2025-09-22 15:25:00 +0530

    logical operator and or not.

---
+++ logical_operator.py
@@ -0,0 +1,20 @@
+
+temp = 25
+
+if temp > 20 and temp < 30:
+  print("Temperature is good.")
+else:
+  print("Temperature is bad.")
+
+
+if temp <= 0 or temp >= 30:
+  print("Temperature is bad.")
+else:
+  print("Temperature is good.")
+
+sunny = True
+
+if not sunny:
+  print("It's sunny outside.")
+else:
+  print("It's cloudy outside.")
