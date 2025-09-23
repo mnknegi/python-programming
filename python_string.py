58785a57f5b2f2ea3a2bdd3921568f8a14e347d5
imayanknegi29 <imayanknegi29@gmail.com>
2025-09-23 23:13:18 +0530

    Python String

---
+++ python_string.py
@@ -0,0 +1,19 @@
+
+# name = input("Enter your name: ")
+phone_number = input("Enter your phone number: ") # 1-234-567-1234
+
+# result = len(name)
+# result = name.find(" ") first occurance
+# result = name.rfind("a")  last occurance
+# result = name.capitalize() capitalize first letter
+# result = name.upper()
+# result = name.isdigit()
+# result = name.isalpha()
+
+# result = phone_number.count("-")
+# phone_number = phone_number.replace("-", " ")
+phone_number = phone_number.replace("-", "")
+
+print(phone_number)
+
+# print(help(str))
