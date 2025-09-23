607313c5adc6c47ff65b7fe5e24fd3ddc08d5bad
imayanknegi29 <imayanknegi29@gmail.com>
2025-09-23 23:41:50 +0530

    Indexing in Python

---
+++ indexing.py
@@ -0,0 +1,24 @@
+# Indexing: accessing elements of the sequence using [ ] (indexing operator)
+# [start : end : step] # start is inclusive, end is exclusive.
+
+credit_card = "1234-5678-9012"
+
+# print(credit_card[0])
+# print(credit_card[4])
+
+# print(credit_card[:4]) # credit_card[0:4]
+# print(credit_card[5:9])
+# print(credit_card[5:]) # credit_card[5:14]
+
+# print(credit_card[-1]) # last element
+# print(credit_card[-2])
+
+# print(credit_card[::2])
+# print(credit_card[2:12:3])
+
+# last_digits = credit_card[-4:]
+# print(f"XXXX-XXXX-{last_digits}")
+
+# reverse the string
+# credit_card = credit_card[::-1]
+# print(credit_card)
