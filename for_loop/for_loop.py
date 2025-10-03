2e7a444346462c97811d22d39b2867fc76728044
imayanknegi29 <imayanknegi29@gmail.com>
2025-10-03 11:38:14 +0530

    for loop introduction

---
+++ for_loop/for_loop.py
@@ -0,0 +1,28 @@
+
+# iterate over a sequence
+# for counter in range(1, 11):
+#   print(counter)
+
+# iterate over a sequence in reverse order
+# for counter in reversed(range(1, 11)):
+#   print(counter)
+
+# print("HAPPY NEW YEAR")
+
+# iterate over a sequence in reverse order with steps
+# for counter in reversed(range(1, 11, 2)):
+#   print(counter)
+
+# use of continue in for loop
+# for counter in range(1, 21):
+#   if counter == 13:
+#     continue
+#   else:
+#     print(counter)
+
+# use of break in for loop
+for counter in range(1, 21):
+  if counter == 13:
+    break
+  else:
+    print(counter)
