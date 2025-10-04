5c26f72d73e5deb0fcf2ab3e4612cfe71d6494b5
imayanknegi29 <imayanknegi29@gmail.com>
2025-10-04 11:56:57 +0530

    collection introduction

---
+++ collection/collection.py
@@ -0,0 +1,63 @@
+
+# ARRAY
+
+# fruits = ["apple", "banana", "mango", "papaya"]
+
+# print(fruits)
+# print(fruits[:3])
+# print(fruits[::2])
+# print(fruits[::-1])
+
+# for fruit in fruits:
+#   print(fruit)
+
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+# print("pineapple" in fruits)
+
+# fruits[0] = "pineapple"
+# print(fruits)
+
+# fruits.append("pineapple")
+# print(fruits)
+
+# fruits.remove("apple")
+# print(fruits)
+
+# fruits.insert(1, "pineapple")
+# fruits.sort()
+
+# fruits.reverse()
+# fruits.clear()
+
+# print(fruits.index("banana"))
+# print(fruits.index("pineapple")) Exception: pineapple is not in list.
+# print(fruits)
+
+# fruits.insert(1, "banana")
+# fruits.insert(5, "banana")
+
+# print(fruits.count("banana"))
+
+#  SET
+
+# fruits = {"apple", "banana", "mango", "papaya"}
+
+# fruits.add("pineapple")
+
+# print(fruits)
+
+# fruits.remove("banana")
+
+# print(fruits)
+
+# fruits.clear()
+
+# TUPLE
+
+fruits = ("apple", "banana", "mango", "banana", "papaya", "banana")
+
+print(fruits.index("banana"))
+print(fruits.count("banana"))
