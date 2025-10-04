51d3a5f5beaea19fdb60740b445a4ce11ad007ef
imayanknegi29 <imayanknegi29@gmail.com>
2025-10-04 13:02:24 +0530

    num pad exercise

--- 2D_collection/2D_collection.py
+++ 2D_collection/2D_collection.py
@@ -1,11 +1,24 @@
 
-groceries = [
-  ["apple", "banana", "pineapple"],
-  ["cucumber", "beans", "ladyfinger"],
-  ["fish", "chichen", "turkey"]
-]
+# groceries = [
+#   ["apple", "banana", "pineapple"],
+#   ["cucumber", "beans", "ladyfinger"],
+#   ["fish", "chichen", "turkey"]
+# ]
 
-for collection in groceries:
-  for food in collection:
-    print(food, end=" ")
+# for collection in groceries:
+#   for food in collection:
+#     print(food, end=" ")
+#   print()
+
+num_pad = ((1, 2, 3), 
+           (4, 5, 6), 
+           (7, 8, 9), 
+           ("*", 0, "#"))
+
+print()
+for row in num_pad:
+  for num in row:
+    print(num, end="    ")
   print()
+
+print()
