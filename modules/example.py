b1859d318b5217084483004df254055060967093
imayanknegi29 <imayanknegi29@gmail.com>
2025-10-07 10:50:14 +0530

    Introduction of modules.

---
+++ modules/example.py
@@ -0,0 +1,10 @@
+
+import geometry
+
+print(geometry.square(2))
+
+print(geometry.cube(2))
+
+print(geometry.circumference(7))
+
+print(geometry.area_of_circle(7))
---
+++ modules/geometry.py
@@ -0,0 +1,14 @@
+
+import math
+
+def square(side):
+  return side * side
+
+def cube(side):
+  return side * side * side
+
+def circumference(radius):
+  return 2 * math.pi * radius
+
+def area_of_circle(radius):
+  return math.pi * radius * radius
